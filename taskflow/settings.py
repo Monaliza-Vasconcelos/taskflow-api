856bfc0088babb6160463fbffabd1c3488e6d69c
Monaliza Vasconcelos <monalizavasconcelos889@gmail.com>
2026-05-16 00:10:26 -0300

    ajustes no banco de dados para rodar no postgree

--- taskflow/settings.py
+++ taskflow/settings.py
@@ -85,8 +85,10 @@ WSGI_APPLICATION = 'taskflow.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
+    'default': dj_database_url.parse(
+        os.environ["DATABASE_URL"],
+        conn_max_age=600,
+        ssl_require=False
     )
 }
 
