d39ee78f33547dc644e981eafdccc1d74711a943
Monaliza Vasconcelos <monalizavasconcelos889@gmail.com>
2026-05-16 00:48:32 -0300

    200~DATABASE_URL

--- taskflow/settings.py
+++ taskflow/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'taskflow.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3')
+        default=os.environ["DATABASE_URL"]
     )
 }
 
